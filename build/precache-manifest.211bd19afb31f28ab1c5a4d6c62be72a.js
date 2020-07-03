74e1fcf9d9ae89e49dc150a43866df5bc1048f76
Mohit <mohit.kumar@zconsolutions.com>
2020-07-03 20:21:04 +0530

    created build

---
+++ build/precache-manifest.211bd19afb31f28ab1c5a4d6c62be72a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06415799f6a35757ec94060515973210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14d9701909b3ed677107",
+    "url": "/static/css/2.a60cc64e.chunk.css"
+  },
+  {
+    "revision": "a7090a57de20e0f26ac1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14d9701909b3ed677107",
+    "url": "/static/js/2.2a10098f.chunk.js"
+  },
+  {
+    "revision": "49fe2bc6ca6e77e8e48c05ef1eac3fbf",
+    "url": "/static/js/2.2a10098f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7090a57de20e0f26ac1",
+    "url": "/static/js/main.fe18f0f7.chunk.js"
+  },
+  {
+    "revision": "529e41fd73da20127301",
+    "url": "/static/js/runtime-main.a6db389b.js"
+  }
+]);
